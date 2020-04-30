d212e77563f7329932061da8d085fdd5b55244b9
Yossi <yosef.d.cohen@gmail.com>
2020-04-29 19:09:54 -0500

    add favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_000038) do
+ActiveRecord::Schema.define(version: 2020_04_30_000851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2020_04_30_000038) do
   create_table "comments", force: :cascade do |t|
     t.string "user_comments"
     t.integer "user_ratings"
+    t.integer "bike_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
     t.integer "bike_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
