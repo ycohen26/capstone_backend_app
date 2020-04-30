017282e4515a5c017bc40d80c7cd7ad4437a990e
Yossi <yosef.d.cohen@gmail.com>
2020-04-29 19:07:58 -0500

    add comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_235344) do
+ActiveRecord::Schema.define(version: 2020_04_30_000038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2020_04_29_235344) do
     t.boolean "original_receipt"
     t.boolean "im_a_shop"
     t.boolean "negotiable_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_comments"
+    t.integer "user_ratings"
+    t.integer "bike_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
